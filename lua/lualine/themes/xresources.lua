1d2e791abfe085d09d99bf32d6bb75ca810f7d67
martineausimon <martineau.simon@gmail.com>
2023-12-16 17:26:39 +0100

    chore(lualine): Improve lualine theme

--- lua/lualine/themes/xresources.lua
+++ lua/lualine/themes/xresources.lua
@@ -2,32 +2,32 @@ local C = require('nvim-xresources.colors')
 
 return {
   normal = {
-    a = { bg = C.cyan, fg = C.black, gui = 'bold' },
+    a = { bg = C.bg2, fg = C.fg1, gui = C.bold },
     b = { bg = C.bg1, fg = C.fg1 },
     c = { bg = C._bg, fg = C.fg }
   },
   insert = {
-    a = { bg = C.magenta, fg = C.black, gui = 'bold' },
+    a = { bg = C.bg2, fg = C.light_blue, gui = C.bold },
     b = { bg = C.bg1, fg = C.fg1 },
     c = { bg = C._bg, fg = C.fg }
   },
   visual = {
-    a = { bg = C.yellow, fg = C.black, gui = 'bold' },
+    a = { bg = C.bg2, fg = C.light_yellow, gui = C.bold },
     b = { bg = C.bg1, fg = C.fg1 },
     c = { bg = C._bg, fg = C.fg }
   },
   replace = {
-    a = { bg = C.red, fg = C.black, gui = 'bold' },
+    a = { bg = C.bg2, fg = C.light_red, gui = C.bold },
     b = { bg = C.bg1, fg = C.fg1 },
     c = { bg = C._bg, fg = C.fg }
   },
   command = {
-    a = { bg = C.blue, fg = C.black, gui = 'bold' },
+    a = { bg = C.bg2, fg = C.blue, gui = C.bold },
     b = { bg = C.bg1, fg = C.fg1 },
     c = { bg = C._bg, fg = C.fg }
   },
   inactive = {
-    a = { bg = C._bg, fg = C.grey, gui = 'bold' },
+    a = { bg = C._bg, fg = C.grey, gui = C.bold },
     b = { bg = C._bg, fg = C.grey },
     c = { bg = C._bg, fg = C.grey }
   }
